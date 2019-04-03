0fdd72ee6e21cf93eeb4438d6126df3b49e7ecd9
Tim Maes <tim.maes@sweetmustard.be>
2019-04-03 13:52:29 +0200

    Enum support for models

--- src/TexTranAsync.Service.Abstractions/Models/CreateUserModel.cs
+++ src/TexTranAsync.Service.Abstractions/Models/CreateUserModel.cs
@@ -2,6 +2,7 @@
 // This file is auto generated. Changes to this file will be lost!
 using System;
 using System.Collections.Generic;
+using TexTranAsync.Data.Abstractions.Entities;
 	
 namespace TextTranAsync.Data.Abstractions.Entities
 {
@@ -13,6 +14,7 @@ namespace TextTranAsync.Data.Abstractions.Entities
 		public string FirstName  { get; set; }
 		public string LastName  { get; set; }
 		public string EmailAddress  { get; set; }
+		public Group Group  { get; set; }
 	}
 }
 
--- src/TexTranAsync.Service.Abstractions/Models/EditUserModel.cs
+++ src/TexTranAsync.Service.Abstractions/Models/EditUserModel.cs
@@ -1,6 +1,7 @@
 // This file is auto generated. Changes to this file will be lost!
 using System;
 using System.Collections.Generic;
+using TexTranAsync.Data.Abstractions.Entities;
 	
 namespace TextTranAsync.Data.Abstractions.Entities
 {
